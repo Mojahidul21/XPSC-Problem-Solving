f593637d1427dd4861db825ec6720f4599ff833a
Mojahidul21 <148482250+Mojahidul21@users.noreply.github.com>
2024-11-03 08:37:05 +0600

    Random A - C are solved
    Total Solved:
    Day1 = 3
    Week2 = 3
    XPSC = 24

---
+++ Week2/Day1/RandomB_MinorChange.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+#define snl cout<<'\n'                  // snl - staring new line
+#define enl <<'\n';                     // enl - ending new line
+#define dnl cout<<'\n';                 // dnl - detached new line
+#define pss <<" "                       // pss - print single space
+#define pyn ?cout<<"YES":cout<<"NO";    // pyn - print yes no
+#define lli long long int               // lli - long long int
+#define con const int c=1e              // con - constant
+#define imx INT_MAX;                    // imx - INT_MAX
+#define imn INT_MIN;                    // imn - INT_MIN
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string s,t;
+    cin>>s>>t;
+    int ans=0;
+    for(int i=0;i<s.size();i++){
+        if(s[i]!=t[i])ans++;
+    }
+    cout<<ans;
+
+    return 0;
+}
