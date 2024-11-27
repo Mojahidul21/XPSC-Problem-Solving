52d11c3840becb4c8cfa91b7ba2aa3d61e68f26d
Mojahidul21 <148482250+Mojahidul21@users.noreply.github.com>
2024-11-27 15:38:58 +0600

    RandomF-H solved and day target achieved

---
+++ Week5/Day4/RandomF_Codeforces_B.BANBAN.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define tourist ios::sync_with_stdio(0);cin.tie(0); // tourist - fast input output
+
+#define pss <<' '                                   // pss - print single space
+#define pnl cout<<'\n';                             // pnl - print new line
+#define tnl if(t)pnl                                // tnl - testcase new line
+
+#define pye cout<<"YES";                            // pye - print YES
+#define pno cout<<"NO";                             // pno - print NO
+#define pyn ?cout<<"YES":cout<<"NO";                // pyn - print YES/NO
+
+#define lli long long                               // lli - long long
+#define imx INT_MAX;                                // imx - INT_MAX
+#define imn INT_MIN;                                // imn - INT_MIN
+#define con const int c=1e                          // con - constant
+
+#define vii vector<int>v(n);\
+            for(int i=0;i<n;i++)cin>>v[i];          // vii - vector integer input
+#define vli vector<lli>v(n);\
+            for(int i=0;i<n;i++)cin>>v[i];          // vli - vector long long input
+
+int main(){
+    tourist
+
+    int t;cin>>t;
+    while(t--){
+        int n;cin>>n;
+        cout<<(n+1)/2;pnl
+        for(int l=1,r=3*n;l<r;l+=3,r-=3)cout<<l pss<<r pss;
+        tnl
+    }
+
+    return 0;
+}
