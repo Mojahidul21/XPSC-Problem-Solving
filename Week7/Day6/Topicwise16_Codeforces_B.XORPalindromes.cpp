7b6cddf54e00cf6fe6328dbc4f7392238761f09e
Mojahidul21 <148482250+Mojahidul21@users.noreply.github.com>
2024-12-13 06:27:08 +0600

    target achieved

---
+++ Week7/Day6/Topicwise16_Codeforces_B.XORPalindromes.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define tourist ios::sync_with_stdio(0);cin.tie(0); // tourist - fast input output
+
+#define pss <<' '                                   // pss - print single space
+#define pnl cout<<'\n';                             // pnl - print new line
+#define tnl if(t)pnl                                // tnl - testcase new line
+
+#define pye cout<<"YES";                            // pye - print YES
+#define pno cout<<"NO";                             // pno - print NO
+#define pyn ?cout<<"YES":cout<<"NO";                // pyn - print YES/NO
+#define pny ?cout<<"NO":cout<<"YES";                // pny - print NO/YES
+
+#define lli long long                               // lli - long long
+#define imx INT_MAX;                                // imx - INT_MAX
+#define imn INT_MIN;                                // imn - INT_MIN
+#define con const int c=1e                          // con - constant
+
+#define vii vector<int>v(n);\
+            for(int i=0;i<n;i++)cin>>v[i];          // vii - vector integer input
+#define vli vector<lli>v(n);\
+            for(int i=0;i<n;i++)cin>>v[i];          // vli - vector long long input
+
+int main(){
+    tourist
+
+    int t;cin>>t;
+	while(t--){
+	    int n,cnt=0;string s;cin>>n>>s;
+	    for(int l=0,r=n-1;l<r;++l,--r)cnt+=s[l]!=s[r]?1:0;
+	    for(int i=0;!(i>n);++i)cout<<(!(i<cnt)&&!(i>n-cnt)&&((n&1)||!((i-cnt)&1))?1:0);
+	    tnl
+	}
+
+    return 0;
+}
