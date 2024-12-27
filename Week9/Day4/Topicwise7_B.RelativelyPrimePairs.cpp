c10bbcd77c85c079638bf8b0b7faf9592c22989b
Mojahidul21 <148482250+Mojahidul21@users.noreply.github.com>
2024-12-27 07:26:00 +0600

    w9d4

---
+++ Week9/Day4/Topicwise7_B.RelativelyPrimePairs.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define tourist ios::sync_with_stdio(0);cin.tie(0); // tourist - fast input output
+
+#define pss <<' '                                   // pss - print single space
+#define lss if(i!=n-1)cout<<' ';                    // lss - !(last single space)
+#define pnl cout<<'\n';                             // pnl - print new line
+#define tnl if(t)pnl                                // tnl - testcase new line
+
+#define pye cout<<"YES";                            // pye - print YES
+#define pno cout<<"NO";                             // pno - print NO
+#define pyn ?cout<<"YES":cout<<"NO";                // pyn - print YES/NO
+#define pny ?cout<<"NO":cout<<"YES";                // pny - print NO/YES
+
+#define lli long long                               // lli - long long
+#define imx INT_MAX;                                // imx - INT_MAX
+#define imn INT_MIN;                                // imn - INT_MIN
+#define con const int c=1e9                         // con - constant
+
+#define aii vector<int>a(n);\
+            for(int i=0;i<n;i++)cin>>a[i];          // aii - a-vector integer input
+#define bii vector<int>b(n);\
+            for(int i=0;i<n;i++)cin>>b[i];          // bii - b-vector integer input
+#define ali vector<lli>a(n);\
+            for(int i=0;i<n;i++)cin>>a[i];          // ali - a-vector long long input
+#define bli vector<lli>b(n);\
+            for(int i=0;i<n;i++)cin>>b[i];          // bli - b-vector long long input
+
+#define gnp goto print;                             // gnp - go n print
+#define pnt print:cout<<                            // pnt - print
+
+#define mod %1000000007                             // mod - modular
+
+int main(){
+    tourist
+
+    lli l,r;cin>>l>>r;
+    pye pnl
+    for(;r-l-1>=0;l+=2){cout<<l pss<<l+1;if(r-l-1)pnl}
+
+    return 0;
+}
