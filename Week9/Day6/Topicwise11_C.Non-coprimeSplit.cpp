f49cdf8ffbb401465966045716d1ae09d9361532
Mojahidul21 <148482250+Mojahidul21@users.noreply.github.com>
2024-12-28 23:50:00 +0600

    w9d6

---
+++ Week9/Day6/Topicwise11_C.Non-coprimeSplit.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define tourist ios::sync_with_stdio(0);cin.tie(0); // tourist - fast input output
+
+#define pss <<' '                                   // pss - print single space
+#define lss if(i!=n-1)cout<<' ';                    // lss - !(last single space)
+#define pnl cout<<'\n';                             // pnl - print new line
+#define tnl if(t)pnl                                // tnl - testcase new line
+
+#define pye cout<<"YES";                            // pye - print YES
+#define pno cout<<"NO";                             // pno - print NO
+#define pyn ?cout<<"YES":cout<<"NO";                // pyn - print YES/NO
+#define pny ?cout<<"NO":cout<<"YES";                // pny - print NO/YES
+
+#define lli long long                               // lli - long long
+#define imx INT_MAX;                                // imx - INT_MAX
+#define imn INT_MIN;                                // imn - INT_MIN
+#define con const int c=1e9                         // con - constant
+#define mod %1000000007                             // mod - modular
+
+#define aii vector<int>a(n);\
+            for(int i=0;i<n;i++)cin>>a[i];          // aii - a-vector integer input
+#define bii vector<int>b(n);\
+            for(int i=0;i<n;i++)cin>>b[i];          // bii - b-vector integer input
+#define ali vector<lli>a(n);\
+            for(int i=0;i<n;i++)cin>>a[i];          // ali - a-vector long long input
+#define bli vector<lli>b(n);\
+            for(int i=0;i<n;i++)cin>>b[i];          // bli - b-vector long long input
+
+#define gnp goto print;                             // gnp - go n print
+#define pnt print:cout<<                            // pnt - print
+
+int t,l,r,a;
+
+bool is_prime(){
+    for(int i=2;i*i<=l;++i){
+        if(!(l%i)){a=i;return 0;}
+    }
+    return 1;
+}
+
+int main(){
+    tourist
+
+    cin>>t;
+    while(t--){
+        cin>>l>>r;
+        if(r<4)cout<<-1;
+        else if(l==r&&is_prime())cout<<-1;
+        else if(l==r)cout<<a pss<<l-a;
+        else if(l%2)cout<<2 pss<<max(2,l-1);
+        else cout<<2 pss<<max(2,l-2);
+        tnl
+    }
+
+    return 0;
+}
