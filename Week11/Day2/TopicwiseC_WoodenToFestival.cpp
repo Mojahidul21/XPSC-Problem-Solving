06f4ff71b2667e95365ae631543d89a47100ae17
Mojahidul21 <148482250+Mojahidul21@users.noreply.github.com>
2025-01-13 15:37:12 +0600

    Add files via upload

---
+++ Week11/Day2/TopicwiseC_WoodenToFestival.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define tourist ios::sync_with_stdio(0);cin.tie(0); // tourist - fast input output
+
+#define pss <<' '                                   // pss - print single space
+#define lss if(i!=n-1)cout<<' ';                    // lss - !(last single space)
+#define pnl cout<<'\n';                             // pnl - print new line
+#define tnl if(t)pnl                                // tnl - testcase new line
+
+#define pye cout<<"YES";                            // pye - print YES
+#define pno cout<<"NO";                             // pno - print NO
+#define pyn ?cout<<"YES":cout<<"NO";                // pyn - print YES/NO
+#define pny ?cout<<"NO":cout<<"YES";                // pny - print NO/YES
+
+#define lli long long                               // lli - long long
+#define imx INT_MAX;                                // imx - INT_MAX
+#define imn INT_MIN;                                // imn - INT_MIN
+#define con const int c=1e9                         // con - constant
+#define mod %1000000007                             // mod - modular
+
+#define aii vector<int>a(n);\
+            for(int i=0;i<n;i++)cin>>a[i];          // aii - a-vector integer input
+#define bii vector<int>b(n);\
+            for(int i=0;i<n;i++)cin>>b[i];          // bii - b-vector integer input
+#define ali vector<lli>a(n);\
+            for(int i=0;i<n;i++)cin>>a[i];          // ali - a-vector long long input
+#define bli vector<lli>b(n);\
+            for(int i=0;i<n;i++)cin>>b[i];          // bli - b-vector long long input
+
+#define gnp goto print;                             // gnp - go n print
+#define pnt print:cout<<                            // pnt - print
+
+int main(){
+    tourist
+
+    int t;cin>>t;
+    while(t--){
+        int n;cin>>n;aii sort(a.begin(),a.end());
+
+        auto ok=[&](int time){
+            int l=0,cnt=1;
+            for(int r=0;r<n;++r){
+                if(a[r]-a[l]>2*time){
+                    l=r,++cnt;
+                    if(cnt>3)return false;
+                }
+            }
+            return true;
+        };
+
+        int l=0,r=1e9,m,ans;
+        while(l<=r)m=l+(r-l)/2,ok(m)?ans=m,r=m-1:l=m+1;
+        cout<<ans;tnl
+    }
+
+    return 0;
+}
