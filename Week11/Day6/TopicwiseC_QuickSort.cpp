77597118a4a176247efa0812a5c4541db2b9fc40
Mojahidul Alam <148482250+Mojahidul21@users.noreply.github.com>
2025-03-06 11:32:54 +0600

    Update TopicwiseC_QuickSort.cpp

--- Week11/Day6/TopicwiseC_QuickSort.cpp
+++ Week11/Day6/TopicwiseC_QuickSort.cpp
@@ -28,36 +28,13 @@ using namespace std;
 #define gnp goto print;                                         // gnp - go n print
 #define pnt print:cout<<                                        // pnt - print
 
+
 tourist
 
     int t;cin>>t;
     while(t--){
-        int n,k;cin>>n>>k;
-
-        int flag=1,cnt=0,track=1;
-        /*
-            1 -> track not found
-            2 -> track found and sequence ok
-            3 -> sequence not ok
-        */
-        for(int i=1,x;i<=n;++i){
-            cin>>x;
-
-            if(x==track)++track;
-            else++cnt;/*
-
-            if(flag==1){
-                if(x!=1)++cnt;
-                else flag=2;
-            }
-            else if(flag==2){
-                if(x!=track+1)flag=3,++cnt;
-            }
-            else++cnt;
-
-            track=x;*/
-        }
-
+        int n,k,x,cnt=0,track=1;cin>>n>>k;
+        while(n--)cin>>x,++(x!=track?cnt:track);
         cout<<cnt/k+bool(cnt%k);tnl
     }
 
